be506ac041400d714a887604a1e2b2b7804e6892
mohsen <mohsen.kianmher88@gmail.com>
2023-08-01 18:44:46 +0330

    encode morse project (edabit chalenge)

---
+++ morse.py
@@ -0,0 +1,24 @@
+# morse code. edabit challenge ==> https://edabit.com/challenge/5bYXQfpyoithnQisa
+# Create a function that takes a string as an argument and returns the Morse code equivalent.
+
+char_to_dots = {
+  'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+  'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+  'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+  'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+  'Y': '-.--', 'Z': '--..', ' ': ' ', '0': '-----',
+  '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+  '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+  '&': '.-...', "'": '.----.', '@': '.--.-.', ')': '-.--.-', '(': '-.--.',
+  ':': '---...', ',': '--..--', '=': '-...-', '!': '-.-.--', '.': '.-.-.-',
+  '-': '-....-', '+': '.-.-.', '"': '.-..-.', '?': '..--..', '/': '-..-.'
+}
+
+def encode_morse(string):
+    x = ''
+    for i in string:
+        for key in char_to_dots:
+            if i==key:
+                x = char_to_dots[key] + ' ' + x
+    print(x)
+encode_morse('EDABBIT CHALLENGE')
